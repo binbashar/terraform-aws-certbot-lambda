e7731c968459511e84d4a31d32b704a72011d346
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-05-15 12:09:02 -0300

    BBL-275 make format has been applied

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -12,8 +12,8 @@ resource "aws_cloudwatch_event_target" "lets_encrypt_timer_target" {
 
 # Give cloudwatch permission to invoke the function
 resource "aws_lambda_permission" "permission" {
-  action = "lambda:InvokeFunction"
+  action        = "lambda:InvokeFunction"
   function_name = module.certbot_lambda_jenkins.function_name
-  principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.certbot_lambda_timer.arn
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.certbot_lambda_timer.arn
 }
